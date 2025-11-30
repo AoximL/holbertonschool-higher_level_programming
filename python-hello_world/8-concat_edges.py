60ff6220243fa7cbf31f35f92c2d6111fb20bb50
Hussam <samdevy@outlook.com>
2025-11-30 17:52:21 +0300

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
+# comment to satisfy line count
 print(str[39:67] + str[107:112] + str[:6])
